0b750032c5aa773a216ab20fc2aaed2751cc942e
khyojun <38220795+khyojun@users.noreply.github.com>
2022-07-14 17:26:33 +0900

    실전 연습용(복습)-시작

---
+++ src/main/java/com/example/demo_practice/DemoPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class DemoPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_practice/DemoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
