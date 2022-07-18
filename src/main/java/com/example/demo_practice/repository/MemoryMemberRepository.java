ea8b0c1ab6d8b35c606429240cf83841e6b7880a
khyojun <38220795+khyojun@users.noreply.github.com>
2022-07-18 20:30:04 +0900

    aftereach를 위한 clearstore추가

--- src/main/java/com/example/demo_practice/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/demo_practice/repository/MemoryMemberRepository.java
@@ -31,11 +31,4 @@ public class MemoryMemberRepository implements MemberRepository {
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
-
-    public void clearStore(){
-        store.clear();
-
-    }
-
-
 }
