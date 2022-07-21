02afcc202f5df50c25892c9d0c121ce162c760a9
khyojun <38220795+khyojun@users.noreply.github.com>
2022-07-21 20:16:33 +0900

    회원 서비스 구

---
+++ src/main/java/com/example/demo_practice/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.example.demo_practice.service;
+
+import com.example.demo_practice.domain.Member;
+import com.example.demo_practice.repository.MemberRepository;
+import com.example.demo_practice.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+
+    private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    public long join(Member member) {
+
+        // 중복회원 조회
+        ValidateDuplicate(member);
+
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void ValidateDuplicate(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는회원입니다.");
+        });
+
+    }
+    public List<Member> findMembers () {
+        return memberRepository.findAll();
+    }
+
+
+    public Optional<Member> findOneMember(Long memberId){
+        return memberRepository.findById(memberId);
+
+
+    }
+}
