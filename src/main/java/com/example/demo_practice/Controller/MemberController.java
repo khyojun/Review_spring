41518d4028751b3fefbbf55f39ccc7a2042b1f4b
khyojun <38220795+khyojun@users.noreply.github.com>
2022-07-27 22:00:25 +0900

    웹 mvc 회원 등록 구현

--- src/main/java/com/example/demo_practice/Controller/MemberController.java
+++ src/main/java/com/example/demo_practice/Controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.example.demo_practice.Controller;
 
+import com.example.demo_practice.domain.Member;
 import com.example.demo_practice.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
 public class MemberController {
     private final MemberService memberService;
@@ -12,5 +16,29 @@ public class MemberController {
     public MemberController(MemberService memberService){
         this.memberService=memberService;
     }
+
+
+
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createForm";
+    }
+
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+
+        Member member = new Member();
+        member.setName(form.getName());
+        System.out.println("member 가입은?" + member.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+
+    }
+
+
+
 
 }
