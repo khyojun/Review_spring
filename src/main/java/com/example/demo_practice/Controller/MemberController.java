3277f5510f830e6a2a54fbc36014e0de1b161b92
khyojun <38220795+khyojun@users.noreply.github.com>
2022-07-29 19:45:32 +0900

    웹 mvc 회원 조회기능 구현

--- src/main/java/com/example/demo_practice/Controller/MemberController.java
+++ src/main/java/com/example/demo_practice/Controller/MemberController.java
@@ -4,8 +4,12 @@ import com.example.demo_practice.domain.Member;
 import com.example.demo_practice.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -38,6 +42,14 @@ public class MemberController {
 
     }
 
+
+
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 
 
 
